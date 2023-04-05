39132687960a4a1069049a80ebac4ea33685be00
lilingzhi2718 <lilingzhi27118@163.com>
2023-04-06 01:27:55 +0800

    添加销售信息模型与视图文件

--- local-addons/cxrs/models/sell.py
+++ local-addons/cxrs/models/sell.py
@@ -9,26 +9,17 @@ class sell(models.Model):
 
     #名称 货品编号 供应者编号 成本 采购时间 详情
 
-    product_name = fields.Char(string='货品名称',required=True)
     sell_id = fields.Char(string='销售订单编号',readonly=True)
-    sell_num = fields.Float(string='货品数量',default=1)
-    product_img = fields.Binary(string='货品图片')
-    sell_money=fields.Float(string='物品总价',compute='count_total',store=True,readonly=True,digits=(8,1))
+    product_name = fields.Char(string='货品名称', required=True)
+    sell_num = fields.Float(string='销售数量',default=1)
+    sell_cost = fields.Float(string='销售单价', required=True, digits=(8, 1))
+    sell_money=fields.Float(string='销售总价',compute='count_total',store=True,readonly=True,digits=(8,1))
     supplier=fields.Many2many('res.partner',string='购买者')
-    product_sell = fields.Float(string='售出单价',required=True,digits=(8,1))
+    product_img = fields.Binary(string='货品图片')
+
+
     sell_date = fields.Date(string='销售时间')
     sell_detail = fields.Text(string='详情')
-    sell_state=fields.Selection([('one','草稿'),('two','确认订单'),('three','支付成功')],string='销售状态'
-                                   ,readonly=True,default='none',copy=False,track_visiblity='onchange')
-    def button_one(self):
-        return self.write({"product_state":"one"})
-    def button_two(self):
-        return self.write({"product_state":"two"})
-    def button_three(self):
-        return self.write({"product_state":"three"})
-
-
-
 
 
     @api.model
@@ -39,6 +30,6 @@ class sell(models.Model):
 
 
 
-    @api.depends('sell_num','product_sell')
+    @api.depends('sell_num','sell_cost')
     def count_total(self):
-        self. sell_money = self.sell_num * self.product_sell
+        self. sell_money = self.sell_num * self.sell_cost
