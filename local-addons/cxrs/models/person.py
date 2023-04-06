2cf041e3c870460a9417ec775caf6b70ed817a65
lilingzhi2718 <lilingzhi27118@163.com>
2023-04-06 22:47:45 +0800

    优化人物信息

--- local-addons/cxrs/models/person.py
+++ local-addons/cxrs/models/person.py
@@ -4,9 +4,11 @@ class person(models.Model):
     _name = 'cxrs.person'
     _description='人物信息'
     _rec_name = 'person_ids'
-    purchase_id=fields.One2many('cxrs.purchase','person_id',string='采购物品')
-    person_ids = fields.Char(string='人物编号', readonly=True)
+    purchase_id=fields.One2many('cxrs.purchase','person_id',string='收购物品')
     person_name = fields.Char(string='人物名称', required=True)
+    person_ids = fields.Char(string='人物编号', readonly=True)
+    person_wx = fields.Char(string='人物微信', required=True)
+    person_tel = fields.Char(string='联系方式', required=True)
     person_img = fields.Binary(string='人物图片')
     color=fields.Integer()
 
