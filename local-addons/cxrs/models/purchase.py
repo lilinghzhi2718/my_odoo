18d78cbf1d4e497a5cd9adccf689f0a69e397001
lilingzhi2718 <lilingzhi27118@163.com>
2023-04-06 16:17:03 +0800

    更改了采购模型与采购订单视图页面

--- local-addons/cxrs/models/purchase.py
+++ local-addons/cxrs/models/purchase.py
@@ -7,19 +7,23 @@ class purchase(models.Model):
     _rec_name = 'purchase_id'
 
 
-    #名称 货品编号 供应者编号 成本 采购时间 详情
-
+    purchase_id = fields.Char(string='采购订单编号', readonly=True)
     product_name = fields.Char(string='货品名称',required=True)
-    purchase_id = fields.Char(string='采购订单编号',readonly=True)
-    purchase_num = fields.Float(string='采购数量',default=1)
     product_img = fields.Binary(string='货品图片')
-    purchase_money=fields.Float(string='采购总价',compute='count_total',store=True,readonly=True,digits=(8,1))
-    supplier=fields.Many2many('res.partner',string='供应者')
-    purchase_cost = fields.Float(string='货品成本',required=True,digits=(8,1))
-    purchase_date = fields.Date(string='采购时间')
+    purchase_num = fields.Float(string='采购数量',default=1)
+    purchase_cost = fields.Float(string='采购单价', required=True, digits=(8, 1))
+    purchase_money = fields.Float(string='采购总价',compute='count_total',store=True,readonly=True,digits=(8,1))
+
+    supplier = fields.Many2many('res.partner',string='供应者')
+
+    purchase_date = fields.Datetime(string='采购时间',default=fields.Datetime.today())
     purchase_detail = fields.Text(string='采购详情')
-    purchase_state=fields.Selection([('one','草稿'),('two','已验证'),('three','转入库')],string='货品状态'
+
+
+    purchase_state = fields.Selection([('one','草稿'),('two','已验证'),('three','订单完成')],string='货品状态'
                                    ,readonly=True,default='one',copy=False,track_visiblity='onchange')
+
+
     def button_one(self):
         return self.write({"purchase_state":"one"})
     def button_two(self):
@@ -41,3 +45,8 @@ class purchase(models.Model):
     @api.depends('purchase_num','purchase_cost')
     def count_total(self):
         self.purchase_money = self.purchase_num * self.purchase_cost
+
+
+
+
+
